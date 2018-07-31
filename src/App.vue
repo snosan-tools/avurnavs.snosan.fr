e39191d8f86bea280f288437adf8a0db907eaffd
Antoine Augusti <antoine.augusti@developpement-durable.gouv.fr>
2018-07-31 15:06:39 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="map">
+    <component :is="currentView"></component>
+  </div>
+</template>
+
+<script>
+import AVURNAVs from './components/AVURNAVs';
+
+export default {
+  name: 'app',
+  components: {
+    AVURNAVs
+  },
+  data () {
+    return {
+      currentView: AVURNAVs
+    }
+  }
+}
+</script>
+
+<style>
+@import "../node_modules/leaflet/dist/leaflet.css";
+
+body {
+  padding: 0;
+  margin: 0;
+}
+html, body, .map {
+  height: 100%;
+  width: 100%;
+}
+</style>
---
+++ src/components/AVURNAVs.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="map">
+    <l-map :zoom="zoom" :center="center">
+      <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+      <l-marker v-for="marker in markers" :key="marker.id"
+        :visible="marker.visible"
+        :lat-lng="marker.position">
+        <l-popup :content="marker.tooltip" />
+      </l-marker>
+    </l-map>
+  </div>
+</template>
+
+<script>
+import { LMap, LTileLayer, LMarker, LPopup } from 'vue2-leaflet';
+import axios from 'axios';
+
+export default {
+  name: 'example',
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LPopup
+  },
+  data () {
+    return {
+      show: true,
+      zoom: 6,
+      center:[48, -1.219482],
+      url:'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+      attribution:'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>',
+      markers: [],
+    }
+  },
+  methods: {
+    cleanDate: function(date) {
+      if (date === null) {
+        return 'Inconnue'
+      }
+      return date
+    }
+  },
+  created() {
+    ["atlantique", "manche", "méditerranée"].forEach(region => {
+      axios.get(`https://avurnav.antoine-augusti.fr/avurnavs/regions/${region}`).then(response => {
+        response.data.forEach(el => {
+          this.markers.push({
+            position: {
+              lng: el['longitude'],
+              lat: el['latitude']
+            },
+            visible: true,
+            tooltip: `
+              <b>${el['title']}</b>
+              <br/><br/>
+              ${el['content']}
+              <br/><br/>
+              Plus d'informations : <a href="${el['url']}">site de la préfecture maritime</a>.
+              <br/><br/>
+              Date de début : ${this.cleanDate(el['valid_from'])}<br/>
+              Date de fin : ${this.cleanDate(el['valid_until'])}<br/>
+              `
+          })
+        })
+    })
+    })
+  },
+}
+</script>
